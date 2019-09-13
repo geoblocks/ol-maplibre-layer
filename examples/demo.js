f93fa308ce412d46286fb409ef669a3052f32ba6
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2019-09-13 16:27:16 +0200

    Initial commit

---
+++ examples/demo.js
@@ -0,0 +1,34 @@
+import 'ol/ol.css';
+import './style.css';
+
+import Map from 'ol/Map.js'
+
+import View from 'ol/View.js';
+import TileLayer from 'ol/layer/Tile.js';
+import {OSM, TileDebug} from 'ol/source.js';
+
+import MapBoxLayer from '../src/mapbox/index.js';
+
+const mapBoxStyle = 'https://vectortiles-staging.geoportail.lu/styles/roadmap/style.json'
+
+const osmSource = new OSM();
+
+window.map = new Map({
+  layers: [
+    window.mbl = new MapBoxLayer({
+      style: mapBoxStyle,
+      container: 'map'
+    }),
+    new TileLayer({
+      source: new TileDebug({
+        projection: 'EPSG:3857',
+        tileGrid: osmSource.getTileGrid()
+      })
+    })
+  ],
+  target: 'map',
+  view: new View({
+    center: [668584, 6408478],
+    zoom: 10
+  })
+});
---
+++ src/mapbox/index.js
@@ -0,0 +1,84 @@
+import Layer from 'ol/layer/Layer'
+import {toDegrees} from 'ol/math';
+import {toLonLat} from 'ol/proj.js';
+
+import mapboxgl from 'mapbox-gl';
+
+
+export default class MapBox extends Layer {
+
+  constructor(options) {
+
+    const baseOptions = Object.assign({}, options);
+
+    delete baseOptions.accessToken;
+    delete baseOptions.style;
+    delete baseOptions.container;
+    super(baseOptions);
+
+    if (options.accessToken) {
+      mapboxgl.accessToken = options.accessToken;
+    }
+
+    this.map_ = new mapboxgl.Map({
+      container: options.container,
+      style: options.style,
+      attributionControl: false,
+      boxZoom: false,
+      doubleClickZoom: false,
+      dragPan: false,
+      dragRotate: false,
+      interactive: false,
+      keyboard: false,
+      pitchWithRotate: false,
+      scrollZoom: false,
+      touchZoomRotate: false
+    });
+  }
+
+  render(frameState) {
+    const canvas = this.map_.getCanvas();
+    const viewState = frameState.viewState;
+
+    const visible = this.getVisible();
+    canvas.style.display = visible ? 'block' : 'none';
+
+    const opacity = this.getOpacity().toString();
+    if (opacity !== canvas.style.opacity) {
+      canvas.style.opacity = opacity;
+    }
+
+    // adjust view parameters in mapbox
+    const rotation = viewState.rotation;
+    if (rotation) {
+      this.map_.rotateTo(toDegrees(-rotation), {
+        animate: false
+      });
+    }
+    this.map_.jumpTo({
+      center: toLonLat(viewState.center),
+      zoom: viewState.zoom - 1,
+      animate: false
+    });
+
+    // cancel the scheduled update & trigger synchronous redraw
+    // see https://github.com/mapbox/mapbox-gl-js/issues/7893#issue-408992184
+    // NOTE: THIS MIGHT BREAK WHEN UPDATING MAPBOX
+    if (this.map_._frame) {
+      this.map_._frame.cancel();
+      this.map_._frame = null;
+    }
+    this.map_._render();
+
+    return canvas;
+  }
+
+  /**
+   * @param {string} name
+   * @param {boolean} visible
+   */
+  setLayerVisibility(name, visible) {
+    this.map_.setLayoutProperty(name, 'visibility', visible ? 'visible' : 'none');
+  }
+
+}
