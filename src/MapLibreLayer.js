98887defc79ebff1caed7727ef54fa3a156219fa
Frederic Junod <frederic.junod@camptocamp.com>
2022-02-23 14:53:08 +0100

    Generate typescipt types

--- src/MapLibreLayer.js
+++ src/MapLibreLayer.js
@@ -4,9 +4,30 @@ import {toLonLat} from 'ol/proj';
 
 import maplibregl from 'maplibre-gl';
 
+
 /**
  * @typedef {Object} Options
- * @property {Object<string, *>} maplibreOptions
+ * @property {string} [className='ol-layer'] A CSS class name to set to the layer element.
+ * @property {number} [opacity=1] Opacity (0, 1).
+ * @property {boolean} [visible=true] Visibility.
+ * @property {import('ol/extent').Extent} [extent] The bounding extent for layer rendering.  The layer will not be
+ * rendered outside of this extent.
+ * @property {number} [zIndex] The z-index for layer rendering.  At rendering time, the layers
+ * will be ordered, first by Z-index and then by position. When `undefined`, a `zIndex` of 0 is assumed
+ * for layers that are added to the map's `layers` collection, or `Infinity` when the layer's `setMap()`
+ * method was used.
+ * @property {number} [minResolution] The minimum resolution (inclusive) at which this layer will be visible.
+ * @property {number} [maxResolution] The maximum resolution (exclusive) below which this layer will be visible.
+ * @property {number} [minZoom] The minimum view zoom level (exclusive) above which this layer will be visible.
+ * @property {number} [maxZoom] The maximum view zoom level (inclusive) at which this layer will be visible.
+ * @property {import('ol/source/Source').default} [source] Source for this layer.  If not provided to the constructor,
+ * the source can be set by calling {@link module:ol/layer/Layer~Layer#setSource layer.setSource(source)} after
+ * construction.
+ * @property {import('ol/PluggableMap').default|null} [map] Map.
+ * @property {import('ol/layer/Layer').RenderFunction} [render] Render function. Takes the frame state as input and is expected to return an
+ * HTML element. Will overwrite the default rendering for the layer.
+ * @property {Object<string, *>} [properties] Arbitrary observable properties. Can be accessed with `#get()` and `#set()`.
+ * @property {import('maplibre-gl').MapOptions} maplibreOptions Options for maplibre-gl.
  */
 
 
@@ -45,6 +66,9 @@ export default class MapLibreLayer extends Layer {
     this.applyOpacity_();
   }
 
+  /**
+   * @private
+   */
   applyOpacity_() {
     const canvas = this.maplibreMap.getCanvas();
     const opacity = this.getOpacity().toString();
@@ -55,17 +79,16 @@ export default class MapLibreLayer extends Layer {
 
   /**
    * @param {import('ol/PluggableMap').FrameState} frameState
-   * @return {HTMLCanvasElement} canvas
+   * @return {HTMLElement} element
    */
   render(frameState) {
     const viewState = frameState.viewState;
 
     // adjust view parameters in maplibre
     this.maplibreMap.jumpTo({
-      center: toLonLat(viewState.center),
+      center: /** @type {[number, number]} */ (toLonLat(viewState.center)),
       zoom: viewState.zoom - 1,
       bearing: toDegrees(-viewState.rotation),
-      animate: false
     });
 
     const maplibreCanvas = this.maplibreMap.getCanvas();
