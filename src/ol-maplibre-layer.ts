e2511b77091e32bfcd78dfdf37a21c28ebd26143
Frederic Junod <frederic.junod@camptocamp.com>
2023-04-27 10:51:20 +0200

    Migrate to typescript

--- src/ol-maplibre-layer.ts
+++ src/ol-maplibre-layer.ts
@@ -3,20 +3,18 @@ import {toDegrees} from 'ol/math';
 import {toLonLat} from 'ol/proj';
 
 import maplibregl from 'maplibre-gl';
+import type {FrameState} from 'ol/Map';
+import type {Options as LayerOptions} from 'ol/layer/Layer';
 
-/**
- * @typedef {Object} Options
- * @property {Object<string, *>} maplibreOptions
- */
+export type MapLibreLayerOptions = LayerOptions & {
+  maplibreOptions: maplibregl.MapOptions;
+};
 
 export default class MapLibreLayer extends Layer {
-  /**
-   * @param {Options} options
-   */
-  constructor(options) {
-    const baseOptions = Object.assign({}, options);
+  maplibreMap: maplibregl.Map;
 
-    delete baseOptions.maplibreOptions;
+  constructor(options: MapLibreLayerOptions) {
+    const {maplibreOptions, ...baseOptions} = options;
 
     super(baseOptions);
 
@@ -26,7 +24,7 @@ export default class MapLibreLayer extends Layer {
     container.style.height = '100%';
 
     this.maplibreMap = new maplibregl.Map(
-      Object.assign({}, options.maplibreOptions, {
+      Object.assign({}, maplibreOptions, {
         container: container,
         attributionControl: false,
         interactive: false,
@@ -37,15 +35,12 @@ export default class MapLibreLayer extends Layer {
     this.applyOpacity_();
   }
 
-  /**
-   * @param {number} opacity
-   */
-  setOpacity(opacity) {
+  override setOpacity(opacity: number) {
     super.setOpacity(opacity);
     this.applyOpacity_();
   }
 
-  applyOpacity_() {
+  private applyOpacity_() {
     const canvas = this.maplibreMap.getCanvas();
     const opacity = this.getOpacity().toString();
     if (opacity !== canvas.style.opacity) {
@@ -53,26 +48,21 @@ export default class MapLibreLayer extends Layer {
     }
   }
 
-  /**
-   * @param {import('ol/Map').FrameState} frameState
-   * @return {HTMLCanvasElement} canvas
-   */
-  render(frameState) {
+  override render(frameState: FrameState): HTMLElement {
     const viewState = frameState.viewState;
 
     // adjust view parameters in maplibre
     this.maplibreMap.jumpTo({
-      center: toLonLat(viewState.center),
+      center: toLonLat(viewState.center) as [number, number],
       zoom: viewState.zoom - 1,
       bearing: toDegrees(-viewState.rotation),
-      animate: false,
     });
 
     const maplibreCanvas = this.maplibreMap.getCanvas();
     if (!maplibreCanvas.isConnected) {
       // The canvas is not connected to the DOM, request a map rendering at the next animation frame
       // to set the canvas size.
-      this.getMapInternal().render();
+      this.getMapInternal()!.render();
     } else if (!sameSize(maplibreCanvas, frameState)) {
       this.maplibreMap.resize();
     }
@@ -83,13 +73,7 @@ export default class MapLibreLayer extends Layer {
   }
 }
 
-/**
- *
- * @param {HTMLCanvasElement} canvas
- * @param {import('ol/Map').FrameState} frameState
- * @return boolean
- */
-function sameSize(canvas, frameState) {
+function sameSize(canvas: HTMLCanvasElement, frameState: FrameState): boolean {
   return (
     canvas.width === Math.floor(frameState.size[0] * frameState.pixelRatio) &&
     canvas.height === Math.floor(frameState.size[1] * frameState.pixelRatio)
